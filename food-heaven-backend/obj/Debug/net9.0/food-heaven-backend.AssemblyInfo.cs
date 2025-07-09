9947843ec77a996eb9034d9b4cd3ff071d1416d4
Kratosllego <206279627+Kratosllego12@users.noreply.github.com>
2025-07-09 15:48:57 -0500

    fix: works correctly

--- food-heaven-backend/obj/Debug/net9.0/food-heaven-backend.AssemblyInfo.cs
+++ food-heaven-backend/obj/Debug/net9.0/food-heaven-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("food-heaven-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29edb0282491604382c22af1cb4311b30ac3ac04")]
 [assembly: System.Reflection.AssemblyProductAttribute("food-heaven-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("food-heaven-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
